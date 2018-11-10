100919469d5bbbb11f462d9f25f08b35bd9c5626
Mesaj2000 <james@James-Simmons-Laptop.localdomain>
2018-11-10 18:40:30 -0500

    Added the files for Here's Johnny

--- picoCTF2018/Heres_Johnny/JohnnySolution.py
+++ picoCTF2018/Heres_Johnny/JohnnySolution.py
@@ -1,25 +1,15 @@
-import crypt
-
-
-'''
-put the hashed password you want to guess as "theirHash"
-you must run in terminal, not IDLE
-it may take a bit
-'''
+from crypt import crypt
 
 theirHash = "$6$HRMJoyGA$26FIgg6CU0bGUOfqFB0Qo9AE2LRZxG8N3H.3BK8t49wGlYbkFbxVFtGOZqVIq3qQ6k0oetDbn2aVzdhuVQ6US."
 
-
 def rockYou():
-    with open('rockyou.txt','r') as f:
+    with open('rockyouabridged.txt','r') as f:
         for guess in f:
             guess = guess.rstrip("\n")
-            myHash = crypt.crypt(guess, '$6$HRMJoyGA')
+            myHash = crypt(guess, '$6$HRMJoyGA')
             if myHash == theirHash:
-                print(guess)
-                return
+				return guess
 
-    print("NOPE!!!")
-    return
+    return "NOPE!!!"
 
-rockYou()
+print(rockYou())
